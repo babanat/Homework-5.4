95ea7a4a47872ff8ff5fb0b666e941ef1c952801
babanat <babanat3@gmail.com>
2024-06-25 18:06:30 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,14 +1,12 @@
-const isNumberN = +prompt('Enter number');
-let i = 2;
-let flag = 1;
-const sq = Math.sqrt(isNumberN);
-while (i <= sq) {
-  if (isNumberN % i === 0) {
- flag = 0;
-    console.log ('Число составное!');
-    break;
-  }
- i +=1;
-}
-if (flag == 1)  
- console.log ('Простое число');
+ const isNumberN = +prompt('Enter number');
+      let flag = 1;
+      const sq = Math.sqrt(isNumberN);
+      for ( let i = 2; i <= sq; i +=1) {
+        if (isNumberN % i === 0) {
+       flag = 0;
+          console.log ('Число составное!');
+          break;i +=1;
+        }
+      }
+      if (flag == 1)  
+       console.log ('Простое число');
